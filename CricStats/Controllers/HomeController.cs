7be46876936129cf297a0f58c8d735a08639f50b
Mustafa <mustafa.hassan@tenpearls.com>
2021-01-08 00:08:51 +0500

    Update notification messages

--- CricStats/Controllers/HomeController.cs
+++ CricStats/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace CricStats.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your stats description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your cricstats contact page.";
 
             return View();
         }
